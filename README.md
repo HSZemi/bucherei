e63580b437ada077de13cea9358fc87985def158
HSZemi <HSZemi@users.noreply.github.com>
2017-12-27 17:15:32 +0100

    Add setup instructions to README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # bucherei
 Bucher suchen, Bucher hinzufugen, Bucher loschen.
+
+# Setup
+ - Given: MySql/MariaDB server; a webserver with php and the mysqli extension
+ - Create a database and user for Bucherei
+ - Create the `buch` table by executing the `CREATE TABLE` command from `create_table.sql` in the database
+ - Copy `dbconfig.php.template` to `dbconfig.php`
+ - Edit `dbconfig.php` and fill in your database host, user, password and database name
+ - Copy the contents of this repository to the directory of your choice on your webserver
+ - You're done! The Bucherei site should now be accessible from your webserver
---
+++ create_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `buch` (
+  `ID` INTEGER AUTO_INCREMENT PRIMARY KEY,
+  `Nummer` varchar(255) COLLATE utf8mb4_german2_ci DEFAULT NULL,
+  `Autor` varchar(255) COLLATE utf8mb4_german2_ci DEFAULT NULL,
+  `Titel` varchar(255) COLLATE utf8mb4_german2_ci DEFAULT NULL,
+  `Sparte` varchar(255) COLLATE utf8mb4_german2_ci DEFAULT NULL,
+  `Erscheinungsjahr` varchar(255) COLLATE utf8mb4_german2_ci DEFAULT NULL,
+  `Verlag` varchar(255) COLLATE utf8mb4_german2_ci DEFAULT NULL,
+  `Beschreibung` text COLLATE utf8mb4_german2_ci,
+  `Bereich` varchar(255) COLLATE utf8mb4_german2_ci DEFAULT NULL
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_german2_ci;
